dc6f0d8e8ba783141ae396f189ff9ae0da411de8
Danny Ramírez <danny.ramrez7@gmail.com>
2022-11-10 13:48:52 -0600

    2:40:15

--- sanity_appleredesign/schemas/schema.js
+++ sanity_appleredesign/schemas/schema.js
@@ -24,7 +24,7 @@ export default createSchema({
     // The following are document types which will appear
     // in the studio.
     product,
-    vendor,
+    //vendor,
     category,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
